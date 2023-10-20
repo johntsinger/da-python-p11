6e69ff5cc30fd8ba867c2ad19369cccbd894329d
John <john.t.singer@gmail.com>
2023-10-20 17:21:31 +0200

    add test for issue clubs should not be able to use more than their points allowed

--- tests/unit_tests/test_purchase_places.py
+++ tests/unit_tests/test_purchase_places.py
@@ -10,6 +10,11 @@ class TestPurchasePlaces(BaseTestCase):
         cls.club = cls.clubs[0]
         cls.club_points = int(cls.club['points'])
         cls.competition = cls.competitions[0]
+        cls.competition_places = int(cls.competition['numberOfPlaces'])
+
+    def tearDown(self):
+        self.competition['numberOfPlaces'] = self.competition_places
+        self.club['points'] = self.club_points
 
     def test_club_points_updated(self):
         places_booked = 5
@@ -24,3 +29,51 @@ class TestPurchasePlaces(BaseTestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(self.club['points'], self.club_points - places_booked)
+
+    def test_purchase_less_places_than_club_has_points(self):
+        places_booked = self.club_points - 5
+        data = {
+            'club': self.club['name'],
+            'competition': self.competition['name'],
+            'places': places_booked
+        }
+        response = self.client.post(
+            self.url,
+            data=data
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            self.club['points'],
+            self.club_points - places_booked
+        )
+        self.assertIn(
+            b'Great-booking complete!',
+            response.data
+        )
+        self.assertTrue(
+            int(self.club['points']) >= 0
+        )
+
+    def test_purchase_more_places_than_club_has_points(self):
+        places_booked = self.club_points + 5
+        data = {
+            'club': self.club['name'],
+            'competition': self.competition['name'],
+            'places': places_booked
+        }
+        response = self.client.post(
+            self.url,
+            data=data
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue(
+            int(self.club['points']) >= 0
+        )
+        self.assertEqual(
+            self.club['points'],
+            self.club_points
+        )
+        self.assertIn(
+            b'You do not have enough points.',
+            response.data
+        )
